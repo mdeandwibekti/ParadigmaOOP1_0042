ce2286743dcb98ed084e88a5b0f2348cf9fe4d7c
mdeandwibekti <mdeandwibekti@gmail.com>
2024-05-21 19:44:01 +0700

    int main new

---
+++ agregasi/agregasi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+#include "anak.h"
+#include "ibu.h"
+
+int main() {
+	ibu* varIbu = new ibu("dini");
+	ibu* varIbu2 = new ibu("novi");
+	anak* varAnak1 = new anak("tono");
+	anak* varAnak2 = new anak("rini");
+	anak* varAnak3 = new anak("dewi");
+	
+	
