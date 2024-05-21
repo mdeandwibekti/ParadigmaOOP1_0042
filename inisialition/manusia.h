558d63e98482ddace0a68a7d5780baf966623783
mdeandwibekti <mdeandwibekti@gmail.com>
2024-05-21 18:28:53 +0700

    class manusia

---
+++ inisialition/manusia.h
@@ -0,0 +1,18 @@
+#ifndef MANUSIA_H
+#define MANUSIA_H
+using namespace std;
+
+class manusia{
+public:
+	string nama;
+	jantung varJantung;
+
+	manusia(string pName)
+		:nama(pName) {
+		cout << name << " hidup\n";
+	}
+	~manusia() {
+		cout << name << "mati\n";
+	}
+};
+#endif
