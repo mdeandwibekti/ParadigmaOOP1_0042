d948e48a687aba0d89b015417499aea037f678af
mdeandwibekti <mdeandwibekti@gmail.com>
2024-05-21 20:13:43 +0700

    int main asosiasi

--- asosiasi/asosiasi.cpp
+++ asosiasi/asosiasi.cpp
@@ -33,3 +33,37 @@ public:
 	void cetakpasien();
 
 };
+
+void pasien::tambahdokter(dokter* pDokter) {
+	daftar_dokter.push_back(pDokter);
+}
+void pasien::cetakdokter() {
+	cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+	for (auto& a : daftar_dokter) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
+
+int main() {
+	dokter* varDokter1 = new dokter("dr.Budi");
+	dokter* varDokter2 = new dokter("dr.Tono");
+	pasien* varPasien1 = new pasien("Andi");
+	pasien* varPasien2 = new pasien("lia");
+
+	varDokter1->tambahpasien(varPasien1);
+	varDokter1->tambahpasien(varPasien2);
+	varDokter2->tambahpasien(varPasien1);
+
+	varDokter1->cetakpasien();
+	varDokter2->cetakpasien();
+	varPasien1->cetakdokter();
+	varPasien2->cetakdokter();
+
+	delete varPasien1;
+	delete varPasien2;
+	delete varDokter1;
+	delete varDokter2;
+
+	return 0;
+}
